b7e7da90c8a083d79bc13d955be3be397aedf08a
Ritesh Goyal <goyalritesh20@gmail.com>
2021-03-06 05:14:51 -0500

    models added

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from accounts import models
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 # Register your models here.
+admin.site.register(models.Student)
+admin.site.register(models.Teacher)
+admin.site.register(models.Subject)
+admin.site.register(User)
--- accounts/models.py
+++ accounts/models.py
@@ -5,3 +5,30 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
+
+
+class Student(User):
+    fees = models.IntegerField()
+    teachers = models.ManyToManyField('Teacher', related_name='students')
+
+    class Meta:
+        verbose_name = 'student'
+        verbose_name_plural = 'students'
+
+
+class Teacher(User):
+    salary = models.IntegerField()
+
+    class Meta:
+        verbose_name = 'teacher'
+        verbose_name_plural = 'teachers'
+
+
+
+class Subject(models.Model):
+    name =  models.CharField(max_length=64)
+    students = models.ManyToManyField('Student', related_name='subjects')
+    teachers = models.ManyToManyField('Teacher', related_name='subjects')
+
+    def __str__(self):
+        return self.name
